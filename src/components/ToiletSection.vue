0ccab786d928497810cadaeb28ea1b007810920b
Attila Szeremi <attila@szeremi.org>
2016-11-18 17:32:24 +0100

    Add image assets

---
+++ src/components/ToiletSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>this is template body</div>
+</template>
+<script>
+  /** @class ToiletSection */
+  export default {
+    data() {
+      return {
+        msg: 'hello vue'
+      }
+    },
+    components: {}
+  }
+</script>
+<style></style>
